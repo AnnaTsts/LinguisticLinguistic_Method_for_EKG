d62c22aa94c88051974fc15f8af7b36325d51b52
annatsytsyluik <ann.tsytsyluik@gmail.com>
2019-11-10 17:48:41 +0200

    simple check for anomaly in result linguistic chain.

--- src/main/java/Services/AnomalySearcher.java
+++ src/main/java/Services/AnomalySearcher.java
@@ -2,7 +2,9 @@ package Services;
 
 import Model.Anomaly;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class AnomalySearcher {
     private AnomalyService anomalyService = new AnomalyService();
@@ -11,7 +13,27 @@ public class AnomalySearcher {
     public void getAnomalyInResult(int idOfResult){
         List<Anomaly> anomalies =anomalyService.findAllAnomaly();
         String currResultString = resultService.findResult(idOfResult).getResultString();
+        getAnomalyInString( currResultString, anomalies);
+    }
 
+    /***
+     * check every symbol in result without changing elements - the simplest algorithm
+     * @param result -string that should be checked
+     * @param anomalies -list of anomaly that can be in linguistic chain
+     * @return map where int - is start index of firs letter from anomaly and anomaly is info about anomaly
+     */
+    public Map<Integer, Anomaly> getAnomalyInString(String result,List<Anomaly> anomalies){
+        Map<Integer, Anomaly> anomalyMap = new LinkedHashMap<>();
+        for (Anomaly anomaly:anomalies
+             ) {
+           for (int i=0;i<result.length()-LinguisticChainService.CHAR_IN_ANOMALY;i++){
+               String currPartFromResult= result.substring(i,i+LinguisticChainService.CHAR_IN_ANOMALY);
+               if(currPartFromResult.equals(anomaly))
+                   anomalyMap.put(i,anomaly);
+           }
+
+        }
+        return anomalyMap;
     }
 }
 
